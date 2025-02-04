847e11a262c96fa37974aa3994f62e5b855cce7e
3djdavid2 <desarrollowebdavid@gmail.com>
2025-02-04 00:07:00 -0300

    ocr a imagen y extrae texto ok

---
+++ index.js
@@ -0,0 +1,40 @@
+import Together from "together-ai";
+import 'dotenv/config';
+import fs from "fs";
+
+const client = new Together({
+    apiKey: process.env.TOGETHER_API_KEY
+});
+
+const getDescriptionPrompt = "cual es el MONTO y el AÑO que aparece en la imagen adjunta?";
+const imagePath = "./imagen-de-texto.png";
+
+// Función para convertir la imagen a base64
+const encodeImage = (imagePath) => {
+    return fs.readFileSync(imagePath, { encoding: "base64" });
+};
+
+const base64Image = encodeImage(imagePath);
+
+const stream = await client.chat.completions.create({
+    model: "meta-llama/Llama-Vision-Free",
+    messages: [
+        {
+            role: "user",
+            content: [
+                { type: "text", text: getDescriptionPrompt },
+                {
+                    type: "image_url",
+                    image_url: {
+                        url: `data:image/png;base64,${base64Image}`,
+                    },
+                },
+            ],
+        },
+    ],
+    stream: true,
+});
+
+for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || "");
+}
